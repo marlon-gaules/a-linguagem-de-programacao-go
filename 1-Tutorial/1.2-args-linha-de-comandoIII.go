5eba294c4fdca79644f8d6669e3d0edfd2516c8c
Marlon <marloncostaesousa@gmail.com>
2024-08-24 14:07:20 -0300

    Subindo argumentos de linha de comando III

---
+++ 1-Tutorial/1.2-args-linha-de-comandoIII.go
@@ -0,0 +1,15 @@
+// Echo2 exibe seus argumentos de linha de comando
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	fmt.Println(os.Args[1:])
+}
